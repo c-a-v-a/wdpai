523f6bc5748ccf30b4559ff223e6610d4f074731
Filip Cebula <filip.cebula@student.pk.edu.pl>
2026-02-01 23:42:38 +0100

    add message board js and css

---
+++ public/scripts/messageboard.js
@@ -0,0 +1,51 @@
+async function getMessages() {
+  const res = await fetch('/api/message', { method: 'GET' });
+
+  if (!res.ok) {
+    alert('Could not connect to the server');
+    return;
+  }
+
+  return await res.json();
+}
+
+function renderTemplates(messages) {
+  const container = document.getElementById('messages-container');
+  const template = document.getElementById('message-card-template');
+
+  container.innerHTML = "";
+
+  for (const message of messages) {
+    const clone = template.content.cloneNode(true);
+    const titleElement = clone.querySelector('.message-title');
+    const authorElement = clone.querySelector('.message-author');
+    const contentElement = clone.querySelector('.message-content');
+    const commentsElement = clone.querySelector('.message-comments');
+
+    titleElement.textContent = message.title;
+    authorElement.textContent = `By: ${message.author_first_name} ${message.author_surname}`;
+    contentElement.textContent = message.content;
+
+    for (const comment of message.comments) {
+      const commentElement = document.createElement('p');
+      commentElement.textContent = `${comment.first_name} ${comment.surname}: ${comment.content}`;
+
+      commentsElement.appendChild(commentElement);
+    }
+
+    container.appendChild(clone);
+  }
+}
+
+async function onload() {
+  renderTemplates(await getMessages());
+};
+onload();
+
+document.getElementById('add-message').onclick = () => {
+  document.getElementById('modal-overlay').classList.add('modal-overlay-visible');
+}
+
+document.getElementById('cancel-action').onclick = () => {
+  document.getElementById('modal-overlay').classList.remove('modal-overlay-visible');
+}
