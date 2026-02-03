0f0a67c9d28f3c6bb1eeb51cdac88ba955507c0d
Filip Cebula <filip.cebula@student.pk.edu.pl>
2026-02-03 22:59:52 +0100

    add logout

--- public/scripts/shared.js
+++ public/scripts/shared.js
@@ -1,6 +1,13 @@
 const menuOpen = document.getElementById('menu');
 const menuClose = document.getElementById('close-menu');
 const sidebar = document.getElementById('sidebar');
+const logoutButton = document.getElementById('log-out');
 
 menuOpen.onclick = () => sidebar.classList.add('sidebar-open');
 menuClose.onclick = () => sidebar.classList.remove('sidebar-open');
+
+logoutButton.onclick = async () => {
+  await fetch('/api/logout', { method: 'POST' });
+
+  window.location.href = "/login";
+}
